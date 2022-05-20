4b41fa356181892805bb72e8c087d54a0a0b49e4
Carl Williams III <c.williams1@medpace.com>
2022-05-20 16:24:22 -0400

    Displaying a List of Products - Complete

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
+import { Product } from './product';
 import { Observable } from 'rxjs';
 
 
@@ -12,9 +13,14 @@ export class ProductService {
   constructor(private _http: Http) { }
 
  private _albumUrl: string = '../assets/album.json';
+ private _productsUrl: string = '../assets/products.json';
   
   getAlbum( id:number ): Observable<Album> {
     return this._http.get(this._albumUrl).map(response => <Album>(response.json()));
-  }
+  };
+
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this._productsUrl).map(response => <Product[]>(response.json()));
+  };
 
 }
